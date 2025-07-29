fc99d6a517d9f128d8b3959975c75113dc9b15d6
BithreenGirlen <152838289+BithreenGirlen@users.noreply.github.com>
2025-07-29 14:01:12 +0900

    Use precompiled header.

---
+++ project/KarinchanPlayer/pch.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include <Windows.h>
+#include <CommCtrl.h>
+#include <shlwapi.h>
+#include <atlbase.h>
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <unordered_map>
+
+#define DX_NON_USING_NAMESPACE_DXLIB
+#include <DxLib.h>
+
+#include <spine/spine.h>
